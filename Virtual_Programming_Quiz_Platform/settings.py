ef14dfaa2243b2b66113c64ccac093c084a2e04a
sunit-mal <sunitmal1832002@gmail.com>
2023-09-11 00:31:01 +0530

    11-09-23

--- Virtual_Programming_Quiz_Platform/settings.py
+++ Virtual_Programming_Quiz_Platform/settings.py
@@ -64,12 +64,12 @@ WSGI_APPLICATION = 'Virtual_Programming_Quiz_Platform.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # DATABASES = {
 #     'default': {
